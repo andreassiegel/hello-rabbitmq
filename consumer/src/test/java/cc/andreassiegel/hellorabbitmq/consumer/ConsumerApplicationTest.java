d660a5634b28fccf5336eb7fe1553c454ffe4968
Andreas Siegel <mail@andreassiegel.de>
2017-10-22 17:28:27 +0200

    Add another application test

---
+++ consumer/src/test/java/cc/andreassiegel/hellorabbitmq/consumer/ConsumerApplicationTest.java
@@ -0,0 +1,16 @@
+package cc.andreassiegel.hellorabbitmq.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ConsumerApplication.class)
+public class ConsumerApplicationTest {
+
+  @Test
+  public void contextLoads() {
+
+  }
+}
