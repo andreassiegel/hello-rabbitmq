f3be881fd4b05c2137b754ee5f5f67c1f823220e
Andreas Siegel <mail@andreassiegel.de>
2017-10-22 10:37:58 +0200

    Add application test

---
+++ producer/src/test/java/cc/andreassiegel/hellorabbitmq/producer/ProducerApplicationTest.java
@@ -0,0 +1,16 @@
+package cc.andreassiegel.hellorabbitmq.producer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProducerApplication.class)
+public class ProducerApplicationTest {
+
+  @Test
+  public void contextLoads() {
+
+  }
+}
